aa01c852a8a0d9a347891040ca1027d5ac1ffe93
lewisb5746 <lewisb5746@gmail.com>
2022-10-13 16:51:57 +1000

    changed instances of 'destination' to 'event'

--- travel/__init__.py
+++ travel/__init__.py
@@ -25,8 +25,8 @@ def create_app():
     #add Blueprints
     from . import views
     app.register_blueprint(views.mainbp)
-    from . import destinations
-    app.register_blueprint(destinations.bp)
+    from . import events
+    app.register_blueprint(events.bp)
     from . import auth
     app.register_blueprint(auth.bp)
 
