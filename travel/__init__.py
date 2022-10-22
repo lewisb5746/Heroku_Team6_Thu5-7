d50fccdecee56582f351dd6278a4bf8cee2249db
lewisb5746 <lewisb5746@gmail.com>
2022-10-22 17:56:40 +1000

    events fix

--- travel/__init__.py
+++ travel/__init__.py
@@ -39,8 +39,8 @@ def create_app():
     #add Blueprints
     # from . import views
     # app.register_blueprint(views.mainbp)
-    #from . import events
-    #app.register_blueprint(events.bp)
+    from . import events
+    app.register_blueprint(events.bp)
     #from . import auth
     #app.register_blueprint(auth.bp)
 
